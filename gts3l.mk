f3a25428093cc25a98a839679f008c0ef503bf4b
Jakub Pawlowski <jpawlowski@google.com>
2021-04-13 11:00:12 +0200

    gts3l-common: Use bluetooth.audio@2.1
    bluetooth.audio@2.1 adds new functionalities required for Bluetooth LE
    Audio development
    
    Bug: 150670922
    Change-Id: Iac2ece0c029a38fef82e81f5ba3ba491163818ef

--- gts3l.mk
+++ gts3l.mk
@@ -87,6 +87,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-impl:32 \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.service \
+    android.hardware.bluetooth.audio@2.1-impl:32 \
     audio.bluetooth.default \
     audio.primary.msm8996 \
     audio.r_submix.default \

14b55915f955d292b7879db3f75823618436efeb
Deokgyu Yang <secugyu@gmail.com>
2022-02-17 15:55:13 +0900

    gts3l-common: light: Update light AIDL
    As per this commit:
    https://review.lineageos.org/c/LineageOS/android_hardware_samsung/+/319373
    
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>
    Change-Id: Ic4fe81b530ef4ade9ac55d578a0352fd46ee0085

--- gts3l.mk
+++ gts3l.mk
@@ -256,7 +256,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.gts3l
+    android.hardware.light-service.gts3l
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
--- light/service.cpp
+++ light/service.cpp
@@ -1,54 +1,27 @@
 /*
- * Copyright (C) 2019 The LineageOS Project
+ * Copyright (C) 2021 The LineageOS Project
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-License-Identifier: Apache-2.0
  */
 
-#define LOG_TAG "android.hardware.light@2.0-service.samsung"
+#define LOG_TAG "android.hardware.light-service.gts3l"
 
+#include "Lights.h"
+
+#include <android/binder_manager.h>
+#include <android/binder_process.h>
 #include <android-base/logging.h>
-#include <hidl/HidlTransportSupport.h>
-#include <utils/Errors.h>
 
-#include "Light.h"
-
-using android::hardware::configureRpcThreadpool;
-using android::hardware::joinRpcThreadpool;
-
-using android::hardware::light::V2_0::ILight;
-using android::hardware::light::V2_0::implementation::Light;
-
-using android::OK;
-using android::sp;
-using android::status_t;
+using ::aidl::android::hardware::light::Lights;
 
 int main() {
-    sp<ILight> light = new Light();
+    ABinderProcess_setThreadPoolMaxThreadCount(0);
+    std::shared_ptr<Lights> lights = ndk::SharedRefBase::make<Lights>();
 
-    configureRpcThreadpool(1, true);
+    const std::string instance = std::string() + Lights::descriptor + "/default";
+    binder_status_t status = AServiceManager_addService(lights->asBinder().get(), instance.c_str());
+    CHECK(status == STATUS_OK);
 
-    status_t status = light->registerAsService();
-
-    if (status != OK) {
-        LOG(ERROR) << "Could not register service for Light HAL";
-        goto shutdown;
-    }
-
-    LOG(INFO) << "Light HAL service is Ready.";
-    joinRpcThreadpool();
-
-shutdown:
-    // In normal operation, we don't expect the thread pool to shutdown
-    LOG(ERROR) << "Light HAL failed to join thread pool.";
-    return 1;
+    ABinderProcess_joinThreadPool();
+    return EXIT_FAILURE; // should not reach
 }

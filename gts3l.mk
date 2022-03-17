f91cd8ae2b5a08ee40f645c0818701b5383aca6c
Deokgyu Yang <secugyu@gmail.com>
2022-03-17 15:27:46 +0900

    gts3l-common: device: Build gatekeeper, keymaster vendor
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>
    Change-Id: Ia4d3c1c1bd7edb11627030781bbb65b0bae4c601

--- gts3l.mk
+++ gts3l.mk
@@ -216,7 +216,8 @@ PRODUCT_PACKAGES += \
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl:64 \
-    android.hardware.gatekeeper@1.0-service
+    android.hardware.gatekeeper@1.0-service \
+    android.hardware.gatekeeper@1.0.vendor
 
 # Healthd
 PRODUCT_PACKAGES += \
@@ -260,7 +261,8 @@ PRODUCT_COPY_FILES += \
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl:64 \
-    android.hardware.keymaster@3.0-service
+    android.hardware.keymaster@3.0-service \
+    android.hardware.keymaster@3.0.vendor
 
 # Lights
 PRODUCT_PACKAGES += \

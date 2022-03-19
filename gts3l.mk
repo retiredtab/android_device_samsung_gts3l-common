dd80273a5fdc23a2cf96811a6b55c684b161e6ce
Deokgyu Yang <secugyu@gmail.com>
2022-03-19 16:22:23 +0900

    gts3l-common: keymaster: Use mdfpp keymaster properly
    Not sure all the files and builds I set is requried but it does work
    
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>
    Change-Id: I4dd69e786f6d8b74f14134b87c58c03199ca2d7e

--- gts3l.mk
+++ gts3l.mk
@@ -220,8 +220,7 @@ PRODUCT_PACKAGES += \
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl:64 \
-    android.hardware.gatekeeper@1.0-service \
-    android.hardware.gatekeeper@1.0.vendor
+    android.hardware.gatekeeper@1.0-service
 
 # Healthd
 PRODUCT_PACKAGES += \
@@ -264,9 +263,10 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0 \
     android.hardware.keymaster@3.0-impl:64 \
     android.hardware.keymaster@3.0-service \
-    android.hardware.keymaster@3.0.vendor
+    libkeymaster3device
 
 # Lights
 PRODUCT_PACKAGES += \

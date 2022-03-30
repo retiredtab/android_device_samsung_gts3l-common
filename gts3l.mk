0e8ebf163c68d10c1a3fcea6cb5ea94d31f1c06c
Deokgyu Yang <secugyu@gmail.com>
2022-03-30 19:24:49 +0900

    gts3l-common: camera: Add wifiloader again
    It helps camera working.
    Refer to this commit:
    0c8728fcab9f73739e5d8733f8719c6c279caa29
    
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>
    Change-Id: I1e28c443fe998058e19c94484244a6b73f7fbe7c

--- gts3l.mk
+++ gts3l.mk
@@ -456,7 +456,8 @@ PRODUCT_PACKAGES += \
     hostapd \
     wpa_supplicant \
     wpa_supplicant.conf \
-    WifiOverlay
+    WifiOverlay \
+    wifiloader
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \

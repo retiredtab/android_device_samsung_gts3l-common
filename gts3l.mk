34be929405a39a916368e91b0ceb1ed4499a9e04
Deokgyu Yang <secugyu@gmail.com>
2022-08-09 15:43:19 +0900

    gts3l-common: keymaster: Add custom Keymaster 3.0 HAL for gts3l
    For the skeymast access permissions.
    
    Refers to: https://gitlab.com/android229/android_hardware_interfaces/-/commit/06fb8163a4758717ebb9077a5b9e471eb48ada86
    
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>
    Change-Id: I4343237efeea14d9f5bd33c1f4eddf1357820938

--- gts3l.mk
+++ gts3l.mk
@@ -262,7 +262,7 @@ PRODUCT_COPY_FILES += \
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl:64 \
-    android.hardware.keymaster@3.0-service
+    android.hardware.keymaster@3.0-service.gts3l
 
 # Lights
 PRODUCT_PACKAGES += \

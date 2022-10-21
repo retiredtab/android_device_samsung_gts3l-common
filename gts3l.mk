af7e2ff44908c4bf8ed60382c968035a0d03e505
Deokgyu Yang <secugyu@gmail.com>
2022-10-21 22:50:55 +0900

    gts3l-common: device: Clean up the bluetooth audio definitions
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>
    Change-Id: I9e8b3df9bc64179644b8bdef265f0c14f91e5ba3

--- gts3l.mk
+++ gts3l.mk
@@ -87,7 +87,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-impl:32 \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.service.gts3l \
-    android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default \
     audio.primary.msm8996 \
     audio.r_submix.default \
@@ -121,7 +120,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.audio@2.0-impl
+    android.hardware.bluetooth.audio-impl
 
 # Camera
 PRODUCT_PACKAGES += \

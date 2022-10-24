e6c30b6a8ef8aa25d09460709cb5c2e2c9946819
Deokgyu Yang <secugyu@gmail.com>
2022-10-25 00:16:29 +0900

    Revert "gts3l-common: Switch BtAudio to AIDL"
    This reverts commit 62b7de925885dae6aa578c94005f188bd7b20708.
    
    Change-Id: I85e30e77bc743473dd4453dc07772e3fbce8743a

--- gts3l.mk
+++ gts3l.mk
@@ -90,6 +90,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-impl:32 \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.service.gts3l \
+    android.hardware.bluetooth.audio@2.1-impl:32 \
     audio.bluetooth.default \
     audio.primary.msm8996 \
     audio.r_submix.default \

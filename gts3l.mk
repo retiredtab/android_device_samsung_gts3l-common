d8d8d6134eafcc420b632055bcde7fa73ce5adae
Gabriel Biren <gbiren@google.com>
2023-03-27 22:33:41 +0000

    gts3l-common: device: Update makefile to use the AIDL Wifi Vendor HAL.
    Bug: 274964641
    Test: Pre-submit tests
    Change-Id: I4d9114377fe75b593bbfe6a59f03d8f5d607d6a1

--- gts3l.mk
+++ gts3l.mk
@@ -403,7 +403,7 @@ PRODUCT_COPY_FILES += \
 
 # WiFi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     libwpa_client \
     hostapd \
     wpa_supplicant \

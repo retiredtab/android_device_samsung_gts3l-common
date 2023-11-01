12baa0453ec38b6ce1f2bbaa2b01e2da4d514ca5
LuK1337 <priv.luk@gmail.com>
2023-11-01 18:11:13 +0100

    gts3l-common: device: Use libutils-v33 for pm-service
    Change-Id: Ia998fa72d8747de18010abe6491d2c374cb7aa0a

--- extract-files.sh
+++ extract-files.sh
@@ -93,6 +93,9 @@ function blob_fixup() {
     lib64/libpixelflinger.so)
         "${PATCHELF}" --replace-needed "libcutils.so" "libcutils-v29.so" "${2}"
         ;;
+    vendor/bin/pm-service)
+        grep -q libutils-v33.so "${2}" || "${PATCHELF}" --add-needed "libutils-v33.so" "${2}"
+        ;;
     esac
 }
 
--- gts3l.mk
+++ gts3l.mk
@@ -398,6 +398,7 @@ PRODUCT_PACKAGES += \
     vndk-sp
 
 PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v33.so \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libcutils-v29.so \
     prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite-v29.so \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v29.so
